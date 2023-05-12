cfb277100ba2b708b2df72be00eaeaed130ab6d1
farbod <farbod@strawberry>
2023-05-12 18:20:10 +0100

    first commit

---
+++ index_my_ip.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This shows my IP
+ * 
+ */
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>My IP</title>
+	<meta charset="UTF-8">
+    <link href="style_my_ip.css" rel="stylesheet">
+</head>
+<body>
+    <div>
+        <br>
+        <br>
+        <center>
+            <b><?php echo $_SERVER['REMOTE_ADDR'] ?></b>
+        </center>
+    </div>
+</body>
+</html>
